95d74e0cf65cc8ebbd184556c752d72ef7c40ec5
Denis <den88000@gmail.com>
2022-12-01 17:19:46 +0300

    Initial Commit

---
+++ iOS7-HW23-Kholodkov Denis/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS7-HW23-Kholodkov Denis
+//
+//  Created by Денис Холодков on 01.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS7-HW23-Kholodkov Denis/iOS7_HW23_Kholodkov_DenisApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS7_HW23_Kholodkov_DenisApp.swift
+//  iOS7-HW23-Kholodkov Denis
+//
+//  Created by Денис Холодков on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS7_HW23_Kholodkov_DenisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
